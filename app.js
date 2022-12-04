079e125deb978ad8f725ee83c3757249437390ba
Ori Aboudi <116178279+OriAboudi@users.noreply.github.com>
2022-12-04 15:58:44 +0200

    ok

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const http = require('http');
 const path = require('path');
 const express = require('express');
+const cors = require('cors');
 
 
 // require modules
@@ -13,6 +14,7 @@ const { routesInit } = require('./routes/confingR');
 require('./db/mongoConnect')
 
 //middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
