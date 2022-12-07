898311e98c360fcdc5a98b45c77af890171c1904
Ori Aboudi <116178279+OriAboudi@users.noreply.github.com>
2022-12-07 19:31:11 +0200

    ok

--- db/mongoConnect.js
+++ db/mongoConnect.js
@@ -7,5 +7,5 @@ async function main() {
   await mongoose.connect(`mongodb+srv://${config.nameDB}:${config.passDB}@cluster0.w3qrn28.mongodb.net/project-NodeJs`);
   console.log('mongodb connect project-NodeJs atlas');
   
-  // use `await mongoose.connect('mongodb://user:password@localhost:27017/test');` if your database has auth enabled
+  
 }
